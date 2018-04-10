4428b53a6639c905fbf0b77ed707d1b7cc004f54
kaisanjay <sanjaykai.1994@example.com>
2018-04-10 19:19:42 +0530

    adding some style login part

--- src/component/Login/Login.js
+++ src/component/Login/Login.js
@@ -4,11 +4,13 @@ import "./Login.css"
 class Login extends React.Component {
 	render() {
 		return (
-			 <div class="grid-x grid-margin-x small-up-2 medium-up-3 large-up-4">
-		          <div class="cell">
+			 <div className="grid-x grid-margin-x small-up-2 medium-up-3 large-up-4 ex1" >
+		          <div className="cell bodyPart">
 		            <h2>Login page </h2>
 		            <label>Username</label>
 		            <input type="text" placeholder="Username" name="username" />
+		            <label>Password</label>
+		            <input type="password" placeholder="password" name="password" />
 		          </div>
 		        </div>
 		        
