53e0bce9ed77a1654dc186946847e0e46e57c7bc
kaisanjay <sanjaykai.1994@example.com>
2018-04-10 19:44:09 +0530

    checking onChange

--- src/component/Login/Login.js
+++ src/component/Login/Login.js
@@ -9,15 +9,20 @@ class Login extends React.Component {
 	Login= ()=> {
 		console.log("Login works")
 	}
+	onChange= (e)=>{
+		this.setState({username:e.target.value});
+		console.log(this.state)
+
+	}
 	render() {
 		return (
 			 <div className="grid-x grid-margin-x small-up-2 medium-up-3 large-up-4 ex1" >
 		          <div className="cell bodyPart">
 		            <h2>Login page </h2>
 		            <label>Username</label>
-		            <input type="text" placeholder="Username" name="username" />
+		            <input type="text" placeholder="Username" name="username" onChange={this.onChange}/>
 		            <label>Password</label>
-		            <input type="password" placeholder="password" name="password" />
+		            <input type="password" placeholder="password" name="password" onChange={this.onChange} />
 		            <input type="submit" className="button" value="Login" onClick={this.Login}/>
 		          </div>
 		        </div>
